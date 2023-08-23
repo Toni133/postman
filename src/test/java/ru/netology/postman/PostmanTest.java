d1bce42d435cfc12a24f802d2e82a8dd136e1774
Anton <toniq133@bk.ru>
2023-08-23 14:57:46 +0300

    first commit

---
+++ src/test/java/ru/netology/postman/PostmanTest.java
@@ -0,0 +1,21 @@
+package ru.netology.postman;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+
+class PostmanTest {
+    @Test
+        void shouldReturnSendData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hi")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("привет"));
+    }
+}
